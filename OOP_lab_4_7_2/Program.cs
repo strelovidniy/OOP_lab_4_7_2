01f7894ea71d41811b446320bfde8e9c5646e4b4
Роман Данилевич <dwordchanel@gmail.com>
2020-05-07 18:36:44 +0300

    Add project files.

---
+++ OOP_lab_4_7_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP_lab_4_7_2
+{
+    class Program
+    {
+        private static int n = int.Parse(Console.ReadLine());
+
+        public static Student[] group = new Student[n];
+
+        private static void Main(string[] args)
+        {
+            Input.Read();
+            Output.Write();
+        }
+    }
+}
